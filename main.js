d171d3c818f08b339210249baaf8a0d0cfe7ff6c
Cutekittycat <81061612+Cutekittycat@users.noreply.github.com>
2021-10-07 15:17:14 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    mustache = loadImage('https://i.postimg.cc/g2Gxv0vr/new-moos.png')
 }
 
 function setup() {
@@ -17,6 +21,8 @@ function gotPoses(results)
      if(results.length > 0)
      {
          console.log(results);
+         noseX = results[0].pose.nose.x;
+         noseY = results[0].pose.nose.y;
          console.log("nose x = " + results[0].pose.nose.x);
          console.log("nose y = " + results[0].pose.nose.y);
      }
@@ -27,7 +33,11 @@ function modelLoaded() {
 }
 
 function draw() {
-    image(video, 0, 0, 450, 450,); 
+    image(video, 0, 0, 450, 450,);
+       //fill(0, 255, 255);
+    //stroke(255, 0, 255);
+    //circle(noseX, noseY, 20);
+    image(mustache, noseX - 75, noseY - 75, 150, 150); 
 }
 
 function take_snapshot(){
